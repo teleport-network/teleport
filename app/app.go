f421957346620f51204bf0df7bc8f50c1240278f
kukoomomo <kukoomomo@gmail.com>
2022-04-26 14:53:23 +0800

    update setcode

--- app/app.go
+++ app/app.go
@@ -873,7 +873,7 @@ func (app *Teleport) Name() string { return app.BaseApp.Name() }
 
 // BeginBlocker updates every begin block
 func (app *Teleport) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
-	if ctx.BlockHeight() == 351000{
+	if ctx.BlockHeight() == 413000 {
 		app.SetEVMCode(ctx, common.HexToAddress(syscontracts.TransferContractAddress), transfercontract.TransferContract.Bin)
 		app.SetEVMCode(ctx, common.HexToAddress(syscontracts.RCCContractAddress), rcccontract.RCCContract.Bin)
 		app.SetEVMCode(ctx, common.HexToAddress(syscontracts.MultiCallContractAddress), multicallcontract.MultiCallContract.Bin)
