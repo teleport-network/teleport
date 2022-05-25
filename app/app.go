0ad83506485d8abc2a6d2c907a114e986c514f12
wenbin <wenbin.shi@gmail.com>
2022-05-25 10:37:08 +0800

    update consensusParams

--- app/app.go
+++ app/app.go
@@ -878,12 +878,12 @@ func (app *Teleport) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) a
 
 // EndBlocker updates every end block
 func (app *Teleport) EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock {
-	if req.Height == 1000000 {
+	if req.Height == 298000 {
 		consensusParam := app.GetConsensusParams(ctx)
 		newConsensusParam := &abci.ConsensusParams{
 			Block: &abci.BlockParams{
-				MaxBytes: 10000000,
-				MaxGas:   20000000,
+				MaxBytes: 12020096,
+				MaxGas:   40000000,
 			}}
 		newConsensusParam.Version = consensusParam.Version
 		newConsensusParam.Validator = consensusParam.Validator
